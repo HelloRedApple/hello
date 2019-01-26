a02ab4168a37a33619cc61b53062f0ca35ed401c
xiaokeshuai <1045495519@qq.com>
2019-01-26 12:25:54 +0800

    Initial commit

---
+++ src/words.java
@@ -0,0 +1,20 @@
+public class words {
+
+    private  String china;
+
+
+    @Override
+    public String toString() {
+        return "words{" +
+                "china='" + china + '\'' +
+                '}';
+    }
+
+    public String getChina() {
+        return china;
+    }
+
+    public void setChina(String china) {
+        this.china = china;
+    }
+}
